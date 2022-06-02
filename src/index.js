d364fc3b35ff2fe4fb7e0de19b8ed3dffec80c9d
Hisamuddin Shaikh <85006343+Hisam-94@users.noreply.github.com>
2022-06-02 17:19:43 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const Name = "Hisamuddin Shaikh";
+const Year = new Date();
+const curYear = Year.getFullYear();
+ReactDom.render(
+  <>
+    <p>Created by {Name}</p>
+    <p>Copywrite {curYear}</p>
+  </>,
+  document.getElementById("root")
+);
+
 //Create a react app from scratch.
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
